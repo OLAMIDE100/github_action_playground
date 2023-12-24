8200c38c620c17604e5fffa14bac7893a19fb222
MideKreative <adesoba5@gmail.com>
2023-12-24 20:07:25 +0200

    test folder effect

--- play.sh
+++ play.sh
@@ -1,6 +1,7 @@
 path="$1"
 echo $CREDENTIALS > gcloud-api-key.json
 export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/gcloud-api-key.json
+export TF_VAR_function_version=${GITHUB_RUN_NUMBER}_${GITHUB_RUN_ATTEMPT}
 echo 'yes' | terraform -chdir=$path init 
 terraform -chdir=$path  plan
 terraform -chdir=$path  apply -auto-approve -input=false
